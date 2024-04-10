7fe6bc38811027972420d26b1b88378dae6a7ccd
AlexanderHagegard <76807794+AlexanderHagegard@users.noreply.github.com>
2024-04-10 22:54:04 +0200

    Fix JS scoll, hero BG and start Client section

--- src/scss/js/scroll.js
+++ src/scss/js/scroll.js
@@ -2,18 +2,18 @@ const headerScroll = () => {
 
     let lastScroll = 0;
     const header = document.querySelector('.site-header__navigation');
-    const headerModifyer = 'site-header__scroll';
+    const headerModifyer = 'site-header--scroll';
 
     window.addEventListener('scroll', () => {
         const currentScroll = window.scrollY;
 
-        // if(window.scrollY > 120) {
-        //     header.classList.add(headerModifyer)
-        // } else {
-        //     header.classList.remove(headerModifyer)
-        // }
+        if(window.scrollY > 100) {
+            header.classList.add(headerModifyer)
+        } else {
+            header.classList.remove(headerModifyer)
+        }
 
-        if (currentScroll > 80 && currentScroll > lastScroll) {
+        if (currentScroll > 100 && currentScroll > lastScroll) {
             header.style.transform = 'translateY(-100%)';
         } else {
             header.style.transform = 'translateY(0)';
